5de90dc76c69ee97c407eda3e81f1c11125fb9a8
Rinat <rinatrinat@MacBook-Air-Rinat.local>
2022-02-18 14:52:47 +0300

    add generator password on django

---
+++ generator/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+import random
+
+
+# Create your views here.
+
+def home(request):
+    return render(request, 'generator/home.html')
+
+
+def password(request):
+    the_password = ''
+    characters = list('abcdefghijklnopqrstuvwxyz')
+    length = int(request.GET.get('length', 12))
+
+    if request.GET.get('uppercase'):
+        characters.extend('ABCDEFGHIJKLMNOPQRSTUVWXYZ')
+    if request.GET.get('numbers'):
+        characters.extend('1234567890')
+    if request.GET.get('special'):
+        characters.extend("'+-/*!&$#?=@<>'")
+
+    for x in range(length):
+        the_password += random.choice(characters)
+
+    return render(request, 'generator/password.html', {'password': the_password})
+
+
+def about(request):
+    return render(request, 'generator/about.html')
